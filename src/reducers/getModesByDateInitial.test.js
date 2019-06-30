55c5ceee2559dfbf0c2b6c6706c7ea52174ec0bd
JBudny <jbudny8@gmail.com>
2019-06-30 14:26:41 +0200

    Small refactoring

--- src/reducers/getModesByDateInitial.test.js
+++ src/reducers/getModesByDateInitial.test.js
@@ -1,5 +1,9 @@
 import reducer from "./getModesByDateInitial";
-import * as actions from "../actions/postActions";
+import {
+  getModesByDateInitialRequested,
+  getModesByDateInitialDone,
+  getModesByDateInitialFailed
+} from "../actions/postActions";
 
 describe("reducer", () => {
   it("should return the initial state", () => {
@@ -7,14 +11,14 @@ describe("reducer", () => {
   });
 
   it("should handle GET_MODES_BY_DATE_INITIAL_REQUESTED", () => {
-    expect(reducer([], actions.getModesByDateInitialRequested())).toMatchSnapshot();
+    expect(reducer([], getModesByDateInitialRequested())).toMatchSnapshot();
   });
 
   it("should handle GET_MODES_BY_DATE_INITIAL_DONE", () => {
-    expect(reducer([], actions.getModesByDateInitialDone())).toMatchSnapshot();
+    expect(reducer([], getModesByDateInitialDone())).toMatchSnapshot();
   });
 
   it("should handle GET_MODES_BY_DATE_INITIAL_FAILED", () => {
-    expect(reducer([], actions.getModesByDateInitialFailed())).toMatchSnapshot();
+    expect(reducer([], getModesByDateInitialFailed())).toMatchSnapshot();
   });
 });
